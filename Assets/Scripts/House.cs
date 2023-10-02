44f0c24f1452136f33bb69826cc404921dc5e977
Brandon McDonald <bamcd0313@gmail.com>
2023-10-01 21:56:23 -0400

    Update House.cs

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class House : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Vector2 _binPosition;
 }
