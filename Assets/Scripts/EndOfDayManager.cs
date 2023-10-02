43a69077a6227478347d47efa6b263d9aa15936b
Davidrodvill <dtrac51@gmail.com>
2023-10-02 02:41:21 -0400

    Added end of day scene, not finished. Also added some sounds to script(not working)

--- Assets/Scripts/EndOfDayManager.cs
+++ Assets/Scripts/EndOfDayManager.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EndOfDayManager : MonoBehaviour
 {
+    int howmuchtrashwegot;
+    public TMP_Text eodresults, binscollectedtoday, binscollectedtotal, totalpiecesoftrashcollectedtoday, trashcollectedtotal;
     public void Start()
     {
         //clear the house list
         HouseInfoStorage.ClearHouseList();
+
+        //text
+        eodresults.text = "End Of Day Results";
+        binscollectedtoday.text = "Bins Collected Today:";
+        binscollectedtotal.text = "Bins Collected Total:";
+        totalpiecesoftrashcollectedtoday.text = "Trash Collected Today:";
+        trashcollectedtotal.text = "Trash Collected Total:";
     }
+
 }
