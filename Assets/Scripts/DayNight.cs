5eb6475b61a07123d5348c3bb5984e612bc546b7
Brandon McDonald <bamcd0313@gmail.com>
2023-10-01 20:24:12 -0400

    work on stuff

--- Assets/Scripts/DayNight.cs
+++ Assets/Scripts/DayNight.cs
@@ -7,29 +7,32 @@ using UnityEngine.SceneManagement;
 public class DayNight : MonoBehaviour
 {
     public GameObject overlay;
+    static float SecondsPerDay = 120;
+    static float SecondsPassed;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        SecondsPassed = 0;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        overlay.SetActive(false);
-        StartCoroutine(FadeAndLoad());
+        //get overlay from the scene
+        overlay = GameObject.Find("Overlay");
+        overlay.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //115+-494x+571x^2
+        int i = (int)(115 - 494 * (SecondsPassed / SecondsPerDay) + (571 * Mathf.Pow(SecondsPassed / SecondsPerDay, 2)));
+        print(i);
+        SecondsPassed += Time.deltaTime;
+        overlay.GetComponent<Image>().color =
+                new Color(0, 0, 0, i / 255f);
+        
         
     }
     
-    IEnumerator FadeAndLoad()
-    {
-        //fade to black
-        overlay.SetActive(true);
-        for(int i = 0; i <= 255f; i += 5)
-        {
-            overlay.GetComponent<Image>().color =
-                new Color(0, 0, 0, i / 255f);
-            yield return new WaitForSeconds(0.1f);
-        }
-
-    }
 }
--- Assets/Scripts/GarbageBinBehaviour.cs
+++ Assets/Scripts/GarbageBinBehaviour.cs
@@ -15,39 +15,97 @@ public class GarbageBinBehaviour : MonoBehaviour
     //the prebads for the trash objects
     [SerializeField] GameObject[] _trashObjects;
 
-    // Start is called before the first frame update
+    public float edgeRadius = 0.05f; // Set your desired edge radius
+    private EdgeCollider2D edgeCollider;
+
     void Start()
     {
+        // Create an EdgeCollider2D and set its points to match the polygonRegion
+        edgeCollider = gameObject.AddComponent<EdgeCollider2D>();
+        edgeCollider.points = GetComponent<PolygonCollider2D>().points;
+        edgeCollider.edgeRadius = edgeRadius;
         SpawnTrash();
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     void SpawnTrash()
     {
-        //pick a number between 0 and 2 to determine the amount of trash to spawn
-        int trashAmount = Random.Range(0, 3);
+        //pick a number between 1 and 2 to determine the amount of trash to spawn
+        int trashAmount = Random.Range(1, 3);
 
         //spawn that amount of trash and stink
         for (int i = 0; i < trashAmount; i++)
         {
             GameObject newObj = Instantiate(_trashObjects[Random.Range(0, _trashObjects.Length)], transform.position, Quaternion.identity);
-            newObj.transform.parent = transform;
+            MoveToTrashBin(newObj);
         }
         for (int i = 0; i < trashAmount; i++)
         {
-            GameObject newObj = Instantiate(_stinkObjects[Random.Range(0, _trashObjects.Length)], transform.position, Quaternion.identity);
-            newObj.transform.parent = transform;
+            Instantiate(_stinkObjects[Random.Range(0, _trashObjects.Length)], transform.position, Quaternion.identity);
         }
+    }
+
+    void MoveToTrashBin(GameObject trash)
+    {
+        while (!IsFullyContainedInZone(trash.GetComponent<Collider2D>()))
+        {
+            //put the trash at some random place and z rotation, it needs to be fully contained in the bounds of this objects collider
+            trash.transform.position = new Vector3(
+                Random.Range(transform.position.x - GetComponent<Collider2D>().bounds.extents.x, transform.position.x + GetComponent<Collider2D>().bounds.extents.x),
+                Random.Range(transform.position.y - GetComponent<Collider2D>().bounds.extents.y, transform.position.y + GetComponent<Collider2D>().bounds.extents.y),
+                transform.position.z);
+
+            //rotate the trash to some random rotation
+            trash.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        }
+        
     }
 
     void MoveToPosition()
     {
         
     }
+
+    bool IsFullyContainedInZone(Collider2D colliderToCheck)
+    {
+        // Get the bounds of the fitZone
+        Bounds fitZoneBounds = this.GetComponent<Collider2D>().bounds;
+        
+
+        // Get the local corners of the colliderToCheck
+        Vector2[] colliderCorners = new Vector2[4];
+        colliderCorners[0] = colliderToCheck.bounds.min;
+        colliderCorners[1] = new Vector2(colliderToCheck.bounds.min.x, colliderToCheck.bounds.max.y);
+        colliderCorners[2] = colliderToCheck.bounds.max;
+        colliderCorners[3] = new Vector2(colliderToCheck.bounds.max.x, colliderToCheck.bounds.min.y);
+
+        /*
+        // Transform to world space
+        for (int i = 0; i < 4; i++)
+        {
+            colliderCorners[i] = colliderToCheck.transform.TransformPoint(colliderCorners[i]);
+        }
+        */
+        // Check if all the corners of the collider are inside the fitZone
+        for (int i = 0; i < 4; i++)
+        {
+            if (!fitZoneBounds.Contains(colliderCorners[i]))
+            {
+                print("not fully contained");
+                return false;
+            }
+        }
+
+        // Check if it's not touching the edge
+        bool isTouchingEdge = Physics2D.IsTouching(colliderToCheck, edgeCollider);
+
+        if (isTouchingEdge)
+        {
+            print("touching edge");
+            return false;
+        }
+
+        return true;
+    }
+
 }
