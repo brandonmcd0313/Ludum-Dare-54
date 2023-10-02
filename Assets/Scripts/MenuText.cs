c86d0405eedd9b1aedff4629cf1b436e91425a33
Davidrodvill <dtrac51@gmail.com>
2023-10-01 20:43:54 -0400

    Buttons

--- Assets/Scripts/MenuText.cs
+++ Assets/Scripts/MenuText.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using UnityEngine.SceneManagement;
 
 public class MenuText : MonoBehaviour
 {
+    public TMP_Text guitext;
+    public GameObject PlayButton;
+    
     // Start is called before the first frame update
     void Start()
     {
+        PlayButton.GetComponent<Button>();
+        PlayButton.SetActive(true);
+        guitext.text = "Welcome To Garbage Sim The Best\t\t\t\t   Game Ever";
         
     }
 
@@ -15,4 +25,9 @@ public class MenuText : MonoBehaviour
     {
         
     }
+    public void PlayGameButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
 }
